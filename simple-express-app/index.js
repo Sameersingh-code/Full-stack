e848b78ee288a429a716234a266aeae64d718c66
Sameer singh <Sameersinghmodanwal@gmail.com>
2025-05-28 15:41:32 +0530

    h2
    alll

--- simple-express-app/index.js
+++ simple-express-app/index.js
@@ -1,18 +1,22 @@
-// index.js
 const express = require('express');
-var path =require('path')
-const app = express();
-const PORT = 3000;
+    var path=require('path')
+    const app = express();
+    const port = 3000;
 
-// Middleware (optional)
-app.use(express.json());
-app.set('view', path.join(__dirname,"views"))
-// Routes
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+    // Set EJS as the view engine
+    app.set('view engine', 'ejs');
+    app.set('views', path.join(__dirname, 'views'));
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+    // Define a route to render an EJS template
+    app.get('/', (req, res) => {
+      // Data to pass to the template
+      
+      // Render the 'index' template with the provided data
+      res.render('index');
+    });
+    
+
+    // Start the server
+    app.listen(port, () => {
+      console.log(`Serverlistening on port ${port}`);
+    });
