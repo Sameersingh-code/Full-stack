2d00c4681c0418c1c50a2c1d3851ef8ebea7c5d5
Sameer singh <Sameersinghmodanwal@gmail.com>
2025-05-28 15:35:43 +0530

    Update index.js

--- simple-express-app/index.js
+++ simple-express-app/index.js
@@ -1,11 +1,12 @@
 // index.js
 const express = require('express');
+var path =require('path')
 const app = express();
 const PORT = 3000;
 
 // Middleware (optional)
 app.use(express.json());
-
+app.set('view', path.join(__dirname,"views"))
 // Routes
 app.get('/', (req, res) => {
   res.send('Hello, World!');
